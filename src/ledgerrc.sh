1848d22d0049b9ff76e774fb3022a06dc4309849
Dennis Lee <dennislwm@yahoo.com.au>
2024-09-06 20:35:36 +0800

    feat: add function to print dates for the last 12 months and new alias for expenses ledger report

--- src/ledgerrc.sh
+++ src/ledgerrc.sh
@@ -7,3 +7,17 @@ alias lbin='ledger -M balance ^income -f'
 alias lbpl='ledger -M balance ^income ^expenses -f'
 alias le='ledger equity -f'
 alias lrli='ledger -M register ^liabilities -f'
+alias lem="ledger reg ^expenses --period-sort '(amount)' -M --begin 2022/06/01 -f Denbrige.ledger --display 'account=~/DL/' --wide"
+
+print_dates_last_12_months() {
+    for ((i=1; i<=12; i++)); do
+        # Calculate the first day of the month
+        first_day=$(date --date="$i months ago" +%Y-%m-01)
+        # Calculate the last day of the month
+        last_day=$(date --date="$(date --date="$i months ago +1 month" +%Y-%m-01) -1 day" +%Y-%m-%d)
+
+        echo "First day: $first_day, Last day: $last_day"
+    done
+}
+
+print_dates_last_12_months
