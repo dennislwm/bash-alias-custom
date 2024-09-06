f4678a4ce471af1ebe1357a88caf8b76314df54b
Dennis Lee <dennislwm@yahoo.com.au>
2024-09-06 20:37:58 +0800

    fix: improve error handling and formatting in print_dates_last_12_months function

--- src/ledgerrc.sh
+++ src/ledgerrc.sh
@@ -10,25 +10,24 @@ alias lrli='ledger -M register ^liabilities -f'
 alias lem="ledger reg ^expenses --period-sort '(amount)' -M --begin 2022/06/01 -f Denbrige.ledger --display 'account=~/DL/' --wide"
 
 print_dates_last_12_months() {
-    local file_name="$1"
+  local file_name="$1"
 
-    if [[ -z "$file_name" ]]; then
-        echo "Error: file_name is an empty string."
-        return 1
-    fi
+  if [[ -z "$file_name" ]]; then
+    echo "Error: file_name is an empty string."
+    return 1
+  fi
+  if [[ ! -f "$file_name" ]]; then
+    echo "Error: file '$file_name' does not exist."
+    return 1
+  fi
+  for ((i=1; i<=12; i++)); do
+    # Calculate the first day of the month
+    first_day=$(date -v-"$i"m -v1d +%Y-%m-01)
+    # Calculate the last day of the month
+    last_day=$(date -v-"$i"m -v1d -v+1m -v-1d +%Y-%m-%d)
 
-    if [[ ! -f "$file_name" ]]; then
-        echo "Error: file '$file_name' does not exist."
-        return 1
-    fi
-    for ((i=1; i<=12; i++)); do
-        # Calculate the first day of the month
-        first_day=$(date -v-"$i"m -v1d +%Y-%m-01)
-        # Calculate the last day of the month
-        last_day=$(date -v-"$i"m -v1d -v+1m -v-1d +%Y-%m-%d)
-
-        echo "First day: $first_day, Last day: $last_day"
-    done
+    echo "First day: $first_day, Last day: $last_day"
+  done
 }
 
 print_dates_last_12_months
